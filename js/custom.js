30bc90e775780c3b67d0b50b18e271ea92a0928a
wo0odychan <wo0ody@kakao.com>
2023-06-27 18:13:42 +0900

    sollago

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,38 @@
 $(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        $('._move').each(function () {
+            if (sct + $(window).innerHeight() - 200 > $(this).offset().top) {
+                $(this).addClass('on')
+            } else {
+                $(this).removeClass('on')
+            }
+        })
+    });
+
+    const mainSlide = new Swiper('.main_slide', {
+        loop: true,
+        parallax: true,
+        speed: 1500,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        slideActiveClass: 'on',
+
+    });
+    $('.main_visual .arrows .left').on('click', function () {
+        mainSlide.slidePrev();
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        mainSlide.slideNext();
+    });
 
 })
